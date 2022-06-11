2101b8ab98fb81414d78f908bc8835661319bb0d
smaranhtmlcssjavaScript <78704480+smaranhtmlcssjavaScript@users.noreply.github.com>
2022-06-11 08:53:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+ocuvideo = "";
+status = "";
+objects = [];
+function preload() {
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResults);
+        for (i=0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("numberofobjects").innerHTML = "Number of objects detected are: " + objects.length;
+            fill(255,0,0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded() {
+    console.log("model is loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
